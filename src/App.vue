85b941be449f7440e7473f18c13458f9159ffd08
George Volokitin <saintgeo23@gmail.com>
2019-12-15 20:55:55 +0300

    WTHR-02. Added favicon

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Weather from './components/Weather'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'App',
+
+  components: { Weather }
 }
 </script>
 
+<template>
+  <div id="app" class="app">
+    <Weather />
+  </div>
+</template>
+
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.app {
+  padding: 0;
+  margin: 0;
+  font-family: Arial;
+  list-style: none;
 }
 </style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: 'Weather'
+}
+</script>
+
+<template>
+  <div class="weather">
+    Weather
+  </div>
+</template>
